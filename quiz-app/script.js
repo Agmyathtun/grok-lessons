c7e6029c57ef81967d11f23c8eee218bd0df39f7
Aung Myat Tun <143701212+Agmyathtun@users.noreply.github.com>
2025-12-25 20:16:32 +0700

    quizAppFinished

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -24,6 +24,9 @@ const quizData = [
 let currentQuestion = 0;
 let score = 0;
 
+console.log(currentQuestion);
+
+const quizContainer = document.querySelector('.quiz-container');
 const questionEl = document.getElementById("question");
 const optionsEl = document.getElementById("options");
 const submitBtn = document.getElementById("submit");
@@ -33,4 +36,60 @@ function loadQuestion(){
   const current = quizData[currentQuestion];
   questionEl.innerText = current.question;
   optionsEl.innerHTML = "";
+
+  current.options.forEach(option => {
+    const div = document.createElement("div");
+    div.classList.add("option");
+
+    const input = document.createElement("input");
+    input.type = "radio";
+    input.name = "answer";
+    input.value = option;
+
+    const label = document.createElement("label");
+    label.innerText = option;
+
+    div.appendChild(input);
+    div.appendChild(label);
+    optionsEl.appendChild(div);
+  });
 }
+function getSelected(){
+  const answers = document.querySelectorAll('input[name="answer"]');
+  let selected;
+
+  answers.forEach(answer =>{
+    
+    if(answer.checked){
+      selected = answer.value;
+    }
+  });
+
+  return selected;
+}
+
+submitBtn.addEventListener('click', () =>{
+  const answer = getSelected();
+  if(answer){
+    if(answer === quizData[currentQuestion].correct){
+      score++;
+    }
+    currentQuestion++;
+
+    if(currentQuestion < quizData.length){
+      loadQuestion();
+    }else{
+      showResult();
+    }
+  }
+});
+
+function showResult(){
+  quizContainer.innerHTML = `
+    <h2> Your score ${score} / ${quizData.length}</h2>
+    <button onclick="location.reload()">Reload</button>
+    `
+
+}
+
+loadQuestion();
